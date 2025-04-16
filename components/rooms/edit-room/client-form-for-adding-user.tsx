1abbf159bbfa34f81275e950bf9cd58d3b7843d5
Abhishekh669 <nepaliabhishekh669@gmail.com>
2025-04-16 16:54:21 +0545

    fucking hell error

--- components/rooms/edit-room/client-form-for-adding-user.tsx
+++ components/rooms/edit-room/client-form-for-adding-user.tsx
@@ -5,7 +5,7 @@ import type React from "react"
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import type { Control } from "react-hook-form"
-import type { User } from "@prisma/client"
+import  { User } from "@prisma/client"
 import { Loader2 } from "lucide-react"
 
 interface ClientFormForOthersProps {
